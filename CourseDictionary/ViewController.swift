f3e63405699b70dd05fe585a1ec335c5ffdc0777
Yoli Meydan <meydany@bxscience.edu>
2018-04-11 01:20:33 -0400

    Initial Commit

---
+++ CourseDictionary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CourseDictionary
+//
+//  Created by Yoli Meydan on 4/11/18.
+//  Copyright © 2018 Yoli Meydan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
